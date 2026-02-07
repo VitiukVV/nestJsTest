2627328546798cb8ea5165093971a650680cca05
VolodymyrVitiuk <v.v.endeavour@gmail.com>
2026-02-07 17:03:23 +0100

    add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RestaurantModule } from './restaurant/restaurant.module';
 import Joi from 'joi';
+import { PrismaModule } from './prisma/prisma.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,8 +23,10 @@ import Joi from 'joi';
     }),
 
     RestaurantModule,
+
+    PrismaModule,
+
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
